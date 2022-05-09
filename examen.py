20cf3ab74e43be00ff9a4058da2d81e8fb9bcb78
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-09 18:42:07 +0200

    prueba

---
+++ examen.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pandas.core.frame import DataFrame #esta funcion se encarga de realizar la union de los datos
+
+
+
+
+def dataset_conversiones():
+    datos_conversion=pd.read_csv('conversiones (4).csv', sep=';')                                   
+    datos_conversion= datos_conversion.dropna()
+    print(datos_conversion)
+dataset_conversiones()
+
+def dataset_navegacion():
+    datos_navegacion=pd.read_csv('navegacion (4) (1).csv', sep=';')
+    datos_navegacion= datos_navegacion.dropna()
+    print(datos_navegacion)
+dataset_navegacion()
+
+def separacion_datos_url(datos_navegacion):
+    #En primer lugar creo lista vacias, que luego usaremos para hacer columnas en la tabla
+    campaña=[]
+    adgroup=[]
+    advertisement=[]
+    site_link=[]
+    #Estos datos los vamos a extraer de las urls que se encuentran en la columna de url_landings
+    urls = datos_navegacion['url_landings']
+
+    for url in urls: #En esta parte se separan los datos de la url
+        try:
+             x= str(url).split('camp=') #Se separa la url por el caracter 'camp=', para acceder a este string delante hemos puesto lo de str(url)
+             y= (x[1]).split('&') #ponemos x[1] para que nos deje solo lo que esta después de 'camp='
+             campaña.append(y[0]) #Se añade a la lista de campaña tan solo lo que esta después de 'camp='
+        except:
+            campaña.append(0)
+        
+navegacion(4) (1)['id_campaña']=campaña
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
