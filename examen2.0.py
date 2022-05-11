fac2404ee1b4d4d9b222233a2cdda3a22583fe5f
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-11 16:46:43 +0200

    cambios

--- examen2.0.py
+++ examen2.0.py
@@ -107,16 +107,21 @@ Ordenacion_por_ts()
 
 #PASO 4: UNIR AMBOS DATAFRAMES
 
+#4.1 Antes de unir limpiamos el dataframe de conversiones
 #ahora vamos a limpiar el csv de conversiones 
 def Limpiar_conversiones(dato=[]):
     for i in range(len(dato)): #en este bucle vamos a sustituir los valores 0 por None por blancos
         if dato[i]=='None':
             dato[i]= ''
     return dato
+#Aplicamos la funcion a las columnas 'id_user' y 'gclid'
 Limpiar_conversiones(Dataset_conversiones()['id_user'])
 Limpiar_conversiones(Dataset_conversiones()['gclid'])
 conversion_final = pd.DataFrame({'id_user':Limpiar_conversiones(Dataset_conversiones()['id_user']), 'gclid':Limpiar_conversiones(Dataset_conversiones()['gclid']), 'date':Dataset_conversiones()['date'], 'hour':Dataset_conversiones()['hour'], 'id_lead':Dataset_conversiones()['id_lead'], 'lead_type': Dataset_conversiones()['lead_type'], 'result':Dataset_conversiones()['result']})
 conversion_final.to_csv('conversion_final.csv', sep=';')
+
+
+
 
 
 
